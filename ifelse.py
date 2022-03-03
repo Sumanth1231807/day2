dccee2bb01216ae8e9a44c95aa314853add7d84c
sumanth1231807 <sumanthravi11111@gmail.com>
2022-03-03 11:31:24 +0530

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,5 @@
+a=20
+if a>20:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
---
+++ odd numbers.py
@@ -0,0 +1,8 @@
+n=1
+while(n<15)
+    if n%2==0:
+        print n,"is even."
+    e
+lse:
+        print n,"is odd".
+    n=n+
